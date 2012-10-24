b8d5a39f95b9779772edd1bac2514672625b6e5b
Dave Kelly <dmk3534@gmail.com>
2012-10-24 01:40:44 -0400

    Added templates

--- apoxz/xz/urls.py
+++ apoxz/xz/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('xz.views',
     url(r'^$', 'main'),
+    url(r'^(?P<slug>\S+)/', 'page'),
 )
--- apoxz/xz/views.py
+++ apoxz/xz/views.py
@@ -1,6 +1,16 @@
 # Views.
 
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from apoxz.xz.models import Page
 
 def main(request):
-    return HttpResponse('Alpha Phi Omega - Xi Zeta Chapter')
+    """Handle requests to the base URL of the xz app.
+    
+    This is basically a landing page.
+    """
+    pages = Page.objects.filter(parent__isnull=True, weight__gte=0).order_by('-weight')
+    return render_to_response('landing.html', {pages: pages})
+
+def page(request, slug):
+    page = Page(slug=slug)
