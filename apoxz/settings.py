3951c221a398c1ba95f34a8716bd020016e6009b
Dave Kelly <dmk3534@gmail.com>
2012-10-24 02:47:08 -0400

    static files now handled by nginx

--- apoxz/settings.py
+++ apoxz/settings.py
@@ -60,7 +60,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/var/www/apoxz.org/apoxz/static/'
+STATIC_ROOT = '/var/www/apoxz.org/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -71,7 +71,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    # '/var/www/apoxz.org/apoxz/static',
+    '/var/www/apoxz.org/apoxz/static',
 )
 
 # List of finder classes that know how to find static files in
