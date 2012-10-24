7d410b90c40ff3f3b24c757f673c7b96f971a450
Dave Kelly <dmk3534@gmail.com>
2012-10-24 02:13:01 -0400

    modified filters on main page for site links

--- apoxz/xz/views.py
+++ apoxz/xz/views.py
@@ -1,5 +1,6 @@
 # Views.
-from datetime import datetime
+from datetime import date
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from apoxz.xz.models import Page
@@ -9,12 +10,13 @@ def main(request):
     
     This is basically a landing page.
     """
-    today = datetime.now()
+    today = date.now()
     pages = Page.objects.filter(
             parent__isnull=True,    # Only want root-level pages for front page.
             weight__gte=0,          # Only want visible pages.
-            hide_date__gt=today,    # Make sure page hasn't expired.
             show_date__lte=today    # Make sure page can be published.
+        ).filter(
+            Q(hide_date__gt=today) | Q(hide_date__isnull=True) # Make sure page hasn't expired.
         ).order_by('-weight')       # Follow defined order.
     return render_to_response('landing.html', {pages: pages})
 
